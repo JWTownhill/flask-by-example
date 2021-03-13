851c31003d0a76e8906a0f45a272aa0e4203c1c8
JWTownhill <johntownhill@gmail.com>
2021-03-13 21:38:09 +0000

    env stuff

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
+print(os.environ["APP_SETTINGS"])
 
 
 @app.route("/")
@@ -18,5 +19,3 @@ def hello_name(name):
 
 if __name__ == "__main__":
     app.run()
-
-print(os.environ["APP_SETTINGS"])
